33e00cab84d90f4df073e24dbe824289ee22f690
Zahidul Islam <zahidul.islam1@northsouth.edu>
2021-11-19 02:51:03 +0600

    ass11 server side added

---
+++ data/travelPackages.js
@@ -0,0 +1,60 @@
+
+const packagesData = [
+        {   
+            name: "World Heritage Site, Historical Archaeological Tour Bangladesh & Archaeological Tour Bangladesh", 
+            duration: "3 days",
+            price: "5500",
+            img_url:"https://i.ibb.co/f2JpKC2/paharpur-bihar.jpg"
+        },
+
+        {   
+            name: "Dhaka - Khulna - Sundarban - Dhaka", 
+            duration: "3 days, 4 nights",
+            price: "8000",
+            img_url: "https://i.ibb.co/YPFHJjT/sundarban.jpg"
+        },
+
+        {   
+            name: "Dhaka - Rangamati - Bandarban - Dhaka", 
+            duration: "3 days, 4 nights",
+            price: "6900",
+            img_url: "https://i.ibb.co/M6TQ0MV/rangamati.jpg"
+        },
+
+        {   
+            name: "Dhaka - Sylhet - Srimongal - Dhaka", 
+            duration: "5 days",
+            price: "6500",
+            img_url: "https://i.ibb.co/m53RYSG/sylhet-tea-garden.jpg"
+        },
+
+        {   
+            name: "Dhaka - Bandarban - Dhaka", 
+            duration: "3 days",
+            price: "7500",
+            img_url: "https://i.ibb.co/Nswv6hn/bandarban.jpg"
+        },
+
+        {   
+            name: "Dhaka - Kuakata - Dhaka", 
+            duration: "3 days",
+            price: "6500",
+            img_url: "https://i.ibb.co/6scwCVk/kuakata.jpg"
+        },
+
+        {   
+            name: "Dhaka - Saint Martins - Dhaka", 
+            duration: "5 days",
+            price: "9500",
+            img_url: "https://i.ibb.co/7KqcsSg/saint-martin.jpg"
+        },
+
+        {   
+            name: "Tour To Cox's Bazar", 
+            duration: "3 days",
+            price: "8500",
+            img_url: "https://i.ibb.co/JKmpprr/cox-s-bazar.jpg"
+        }
+];
+
+export default packagesData;
---
+++ index.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+/*Database info
+database user name: travel-booth-bd
+password: e5oW0kDiwiVceG1f
+*/
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0kaol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('Travel-Booth-BD');
+        const travelPackagesCollection = database.collection('travel-packages');
+        const bookingCollection = database.collection('all-bookings');
+ 
+        // post package data in server
+        app.post('/travel-booth-bd/travel-packages', async (req, res) =>{
+        // res.send('hitted the post api...');
+        const package = req.body;
+        const result = await travelPackagesCollection.insertOne(package);
+        res.json(package);
+        // console.log("got the data: ", package);
+        })
+
+        //  read packages data from server
+        app.get('/travel-booth-bd/travel-packages', async (req, res) => {
+            const cursor = travelPackagesCollection.find({})
+            const package = await cursor.toArray();
+            res.send(package);
+        })
+
+        // read selected package data (see details btn)
+        app.get('/travel-booth-bd/travel-packages/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(req);
+            const query = {_id: ObjectId(id)}
+            const selectedPackage = await travelPackagesCollection.findOne(query);
+            res.send(selectedPackage);
+        })
+
+        // post in the server of book-now data
+        app.post('/travel-booth-bd/book-now/', async (req, res) => {
+            res.send('got the booking data...')
+            const bookingData = req.body;
+            console.log('body: ',bookingData);
+            const result = await bookingCollection.insertOne(bookingData);
+            // console.log('result: ',result);
+            res.json(result);
+        }) 
+
+        // see all bookings by logged in email
+        app.get('/travel-booth-bd/all-bookings/:email', async (req, res) => {
+            const loggedEmail = req.params.email;
+            const query = {Booked_Email:loggedEmail}
+            const myAllBookings = await bookingCollection.find(query).toArray();
+            console.log(myAllBookings);
+            res.send(myAllBookings);
+        })
+
+        // see all bookings of all logged users
+        app.get('/travel-booth-bd/all-bookings', async (req, res) => {
+            const cursor = bookingCollection.find({})
+            const allBookings = await cursor.toArray();
+            res.send(allBookings);
+        })
+        // getting request data to update selected field
+        app.get('/travel-booth-bd/select-bookings/:bookingId', async (req, res) => {
+            const selectBooking = req.params.bookingId;
+            const query = {_id: ObjectId(selectBooking)};
+            const result = await bookingCollection.findOne(query);
+            res.send(result);
+            // console.log(selectBooking);
+            // console.log(query);
+        })
+
+        // posting updated data to the server by 
+        app.put('/travel-booth-bd/update-bookings/:bookingId', async (req, res) => {
+            const updateBookingId = req.params.bookingId;
+            console.log('server site: ',updateBookingId);
+            const updatedInfo = req.body;
+            console.log('server site:',updatedInfo)
+            const filter = {_id: ObjectId(updateBookingId)};
+            const options = { upsert: true };
+            const updateDoc = {
+                $set:{
+                    confirmData:{
+                    Contact_Number: updatedInfo.Contact_Number,
+                    Person_Num_Adults: updatedInfo.Person_Num_Adults,
+                    Person_Num_Kids: updatedInfo.Person_Num_Kids
+                    }
+                }
+            }
+            const result = await bookingCollection.updateOne(filter, updateDoc, options)
+            res.send(result)
+            // res.send('updating...')
+            console.log('updated data...of this id: ', updateBookingId)
+        })
+
+        // delete booking 
+        app.delete('/travel-booth-bd/delete-booking/:bookingId', async (req, res) => {
+            console.log(req)
+            const deleteBookingId = req.params.bookingId;
+            console.log(deleteBookingId);
+            const query = {_id: ObjectId(deleteBookingId)}
+            console.log(query);
+            const result = await bookingCollection.deleteOne(query);
+            res.json(result);
+        })
+    } 
+  
+
+    finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+  app.listen(port, () => {
+    console.log("App listening from http://localhost:", port);
+})
