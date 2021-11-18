0972a2604624127e449c30d64220d8af838b2a68
Zahidul Islam <zahidul.islam1@northsouth.edu>
2021-11-19 02:58:51 +0600

    database seceret code removed

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-/*Database info
-database user name: travel-booth-bd
-password: e5oW0kDiwiVceG1f
-*/
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0kaol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
